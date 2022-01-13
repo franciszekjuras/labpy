e197740f1dfbcbe42b0ade6e6dd393e851d333d8
Franciszek Juras <franciszekjuras@gmail.com>
2022-01-13 17:38:22 +0100

    Update to 0.2.1

--- labpy/__init__.py
+++ labpy/__init__.py
@@ -1,9 +1,10 @@
 import os
-dirname = os.path.dirname(__file__)
-try:
-    with open(os.path.join(dirname, '../README.md')) as f:
-        __doc__ = f.read()
-except:
-    pass
 import importlib.metadata
 __version__ = importlib.metadata.version('labpython')
+dirname = os.path.dirname(__file__)
+__doc__ = f"`labpython == {__version__}`\n"
+try:
+    with open(os.path.join(dirname, '../README.md')) as f:
+        __doc__ += f.read()
+except:
+    pass
